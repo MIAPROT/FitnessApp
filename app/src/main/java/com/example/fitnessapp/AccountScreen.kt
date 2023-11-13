876b88936359c344587c4710f2c4548fefd4e96a
Марат <marat18032005@mail.ru>
2023-11-13 12:33:50 +0300

    feat: Добавленны кнопки на экране профиля. Создан экран Калькулятора

--- app/src/main/java/com/example/fitnessapp/AccountScreen.kt
+++ app/src/main/java/com/example/fitnessapp/AccountScreen.kt
@@ -2,13 +2,17 @@ package com.example.fitnessapp
 
 import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
@@ -16,6 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
@@ -27,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -132,8 +138,16 @@ fun AccountScreen() {
                 modifier = Modifier.width(300.dp)
             )
             Text(text = dailyActive.toString())
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.End)) {
-                Text(text = "Сохранить")
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text(text = "Изменить")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Сохранить")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.Face, contentDescription = null)
+                }
             }
         }
     }
